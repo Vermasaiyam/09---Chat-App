1759eaa31b8ee3f91ef7671bc7051c194393c16f
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-15 14:41:09 +0530

    new features added

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
+const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on('connection', (socket)=>{
+    console.log("A new user has been connected", socket.id);
+    socket.on('chat message', (message)=>{
+        console.log("A new user message", message);
+        io.emit('chat message', message);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req, res)=>{
+    res.sendFile('/public/index.html');
+})
 
 server.listen(9000, ()=> console.log(`Server Started at port: 9000`));
